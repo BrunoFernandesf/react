029a9d039e07535843e76639c8a92d2b104edab2
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2025-05-29 15:00:02 -0300

    curso react lista tarefas

---
+++ lista-tarefas/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FunctionalComponent from './components/FunctionalComponent'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World React!</h1>
+      <FunctionalComponent></FunctionalComponent>
+    </>
+  )
+}
+
+export default App
