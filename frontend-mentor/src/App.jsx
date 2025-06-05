8a3cd7fdfcb7a0ec23f8c289710518ed08dba57a
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2025-06-05 14:24:27 -0300

    recipe page conteudo

--- frontend-mentor/src/App.jsx
+++ frontend-mentor/src/App.jsx
@@ -1,5 +1,8 @@
+import Instructions from "./components/recipe-page/Instructions.jsx"
+import Ingredients from "./components/recipe-page/Ingredients.jsx"
 import Intro from "./components/recipe-page/Intro.jsx"
 import Preparation from "./components/recipe-page/Preparation.jsx"
+import Nutrition from "./components/recipe-page/Nutrition.jsx"
 
 function App() {
 
@@ -7,6 +10,9 @@ function App() {
     <>
         <Intro></Intro>
         <Preparation></Preparation>
+        <Ingredients></Ingredients>
+        <Instructions></Instructions>
+        <Nutrition></Nutrition>
     </>
   )
 }
