0af314adedcd7328d08c8eaefcced1c3e5a035c3
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2025-06-05 13:27:17 -0300

    Components Intro e Preparation

---
+++ frontend-mentor/src/components/recipe-page/Preparation.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Preparation = () => {
+    return(
+        <div>
+            <h2>Preparation time</h2>
+            <ul>
+                <li>Total: Approximately 10 minutes</li>
+                <li>Preparation: 5 minutes</li>
+                <li>Cooking: 5 minutes</li>
+            </ul>
+  </div>
+    )
+}
+export default Preparation;
